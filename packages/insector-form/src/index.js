9eff7c0a9958c0a03d5d39dc9603859b6bf94d3a
Martin Söderholm <matte@insector.se>
2017-06-06 14:18:58 +0200

    insector-form 0.1.1

--- packages/insector-form/src/index.js
+++ packages/insector-form/src/index.js
@@ -1,7 +1,27 @@
 import FormModel from './model';
-import {FormTextInput} from './components';
+
+import {ReactInput,
+        ReactTextarea,
+        ReactSelect} from './controls';
+
+import {FormGroup,
+        CheckboxGroup,
+        FormTextInput,
+        FormPasswordInput,
+        FormCheckbox,
+        FormSelect} from './components';
 
 // Exports
 
 export { FormModel };
+
+export { ReactInput };
+export { ReactTextarea };
+export { ReactSelect };
+
+export { FormGroup };
+export { CheckboxGroup };
 export { FormTextInput };
+export { FormPasswordInput };
+export { FormCheckbox };
+export { FormSelect };

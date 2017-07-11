eaa782eac8806bc0751b583232bddb2d67315c18
Martin Söderholm <matte@insector.se>
2017-07-11 13:36:28 +0200

    insector-react-mvc: Removed activeView, activeViewProps, getSubViewModel

--- packages/insector-react-mvc/src/viewmodel.js
+++ packages/insector-react-mvc/src/viewmodel.js
@@ -1,4 +1,4 @@
-import Model, {UNSET_IF_FALSE, SET_SILENT} from 'mozy/model';
+import Model, {UNSET_IF_FALSE} from 'mozy/model';
 
 /**
  * ViewModel, for use with
@@ -34,50 +34,6 @@ export default class ViewModel extends Model {
             this._isFetching = value;
             this.dispatchChange('isFetching');
         }
-    }
-
-    get activeView() {
-        return this.activeViews && this.activeViews[0];
-    }
-
-    get previousActiveView() {
-        const prev = this.getPrevious('activeViews') || [];
-        return prev[0];
-    }
-
-    get activeViews() {
-        return this.get('activeViews');
-    }
-    set activeViews(value) {
-        this.set('activeViews', value, UNSET_IF_FALSE);
-        this.dispatchChange('activeView', this.activeView, this.previousActiveView);
-    }
-
-    get activeViewProps() {
-        return this.get('activeViewProps', {});
-    }
-    set activeViewProps(value) {
-        this.set('activeViewProps', value, UNSET_IF_FALSE);
-    }
-
-    /**
-     * getSubViewModel
-     * @param {mozy.Model} ModelCls Constructor to instantiate
-     * @param {String} instanceKey Key to set on parent model
-     * @param {Object} data Constructor param
-     * @return {mozy.Model} Model instance
-     */
-    getSubViewModel(ModelCls, instanceKey, data = {}) {
-        // Get data
-        const d = this.get(instanceKey, data);
-        // Get model
-        const m = new ModelCls(d);
-        // Set instance key
-        m.instanceKey = instanceKey;
-        // store module instance data on parent Model
-        this.set(instanceKey, m.getModelData(), SET_SILENT);
-        // Return
-        return m;
     }
 
 }

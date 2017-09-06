699361ebb0852ca362d387aba8477db2059b6cb3
Martin Söderholm <matte@insector.se>
2017-09-06 14:01:57 +0200

    insector-react-components: Export createWrapperComponent in index

--- packages/insector-react-components/src/index.js
+++ packages/insector-react-components/src/index.js
@@ -1,10 +1,11 @@
+import createWrapperComponent from './create-wrapper-component';
 import {FAButton,
         FANavItem,
         FALink,
         FAMediaItem} from './fontawesome';
 
 // Exports
-
+export { createWrapperComponent };
 export { FAButton };
 export { FANavItem };
 export { FALink };

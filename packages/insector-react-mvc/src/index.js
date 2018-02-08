1f88cded46a63b5fd40e18aefc97ed440acc2981
Martin Söderholm <matte@insector.se>
2018-02-08 09:50:10 +0100

    insector-react-mvc
    • Removed AppEvent
    • Added wrapView HOC
    • Indentation 4 => 2 in viewmodel

--- packages/insector-react-mvc/src/index.js
+++ packages/insector-react-mvc/src/index.js
@@ -1,12 +1,55 @@
-import AppEvent from './appevent';
+/* eslint indent: ["error", 2, {"SwitchCase": 1}] */
+import React from 'react';
+
 import MVCContainer from './mvccontainer';
 import ReactController from './controller';
 import ReactView from './view';
 import ViewModel from './viewmodel';
 
 // Exports
-export { AppEvent };
 export { MVCContainer };
 export { ReactController };
 export { ReactView };
 export { ViewModel };
+
+/**
+ * wrapView
+ */
+export function wrapView(View, {modelFactory, controllerFactory} = {}) {
+  // Container
+  return class Container extends React.Component {
+
+    constructor(props) {
+      super(props);
+      if (modelFactory) {
+        this.model = modelFactory(this);
+      }
+    }
+
+    render() {
+      return (
+        <View
+          ref="view"
+          model={this.model}
+          {...this.props}
+        />
+      );
+    }
+
+    componentDidMount() {
+      if (controllerFactory) {
+        this.controller = controllerFactory(this);
+      }
+    }
+
+    componentWillUnmount() {
+      if (this.controller) {
+        if (typeof this.controller.dispose === 'function') {
+          this.controller.dispose();
+        }
+        delete this.controller;
+      }
+    }
+
+  };
+}
--- packages/insector-react-mvc/src/viewmodel.js
+++ packages/insector-react-mvc/src/viewmodel.js
@@ -1,3 +1,4 @@
+/* eslint indent: ["error", 2, {"SwitchCase": 1}] */
 import Model from 'mozy/model';
 
 /**
@@ -5,34 +6,34 @@ import Model from 'mozy/model';
  */
 export default class ViewModel extends Model {
 
-    constructor(...args) {
-        super(...args);
-        // Fetching flag
-        this._isFetching = false;
-    }
+  constructor(...args) {
+    super(...args);
+    // Fetching flag
+    this._isFetching = false;
+  }
 
-    get instanceKey() {
-        return this.get('instanceKey');
-    }
-    set instanceKey(value) {
-        this.set('instanceKey', value);
-    }
+  get instanceKey() {
+    return this.get('instanceKey');
+  }
+  set instanceKey(value) {
+    this.set('instanceKey', value);
+  }
 
-    get initialized() {
-        return this.get('initialized', false);
-    }
-    set initialized(value) {
-        this.set('initialized', value, {unsetIfFalsey: true});
-    }
+  get initialized() {
+    return this.get('initialized', false);
+  }
+  set initialized(value) {
+    this.set('initialized', value, {unsetIfFalsey: true});
+  }
 
-    get isFetching() {
-        return this._isFetching;
+  get isFetching() {
+    return this._isFetching;
+  }
+  set isFetching(value) {
+    if (value !== this._isFetching) {
+      this._isFetching = value;
+      this.dispatchChange('isFetching');
     }
-    set isFetching(value) {
-        if (value !== this._isFetching) {
-            this._isFetching = value;
-            this.dispatchChange('isFetching');
-        }
-    }
+  }
 
 }
